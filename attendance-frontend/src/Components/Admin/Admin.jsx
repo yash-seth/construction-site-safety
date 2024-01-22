0f83538119da2b2081395e68d04f31ee6fef473e
Yash Seth <yashseth2002@gmail.com>
2024-01-22 11:47:50 +0530

    chore: add reset query func for admin

--- attendance-frontend/src/Components/Admin/Admin.jsx
+++ attendance-frontend/src/Components/Admin/Admin.jsx
@@ -11,7 +11,7 @@ function Admin() {
     const handleChange = (event) => {
         const value = event.target.value;
         setQuery(value);
-      };
+    };
 
     const queryLLM = () => {
         fetch("http://127.0.0.1:5000/query", {
@@ -23,7 +23,6 @@ function Admin() {
             body: JSON.stringify({query: query})})
         .then((res) =>
             res.json().then((data) => {
-                // Setting a data from api
                 setAnswer({
                     response: data.response
                 });
@@ -39,6 +38,7 @@ function Admin() {
             <input id="admin-query-input"type="text" onChange={handleChange} value={query}/>
         </div>
         <button id="admin-query-btn" onClick={() => queryLLM()}>Query</button>
+        <button id="admin-query-btn" onClick={() => setQuery('')}>Reset</button>
         <h3><label>{answer.response}</label></h3>
     </div>
 
