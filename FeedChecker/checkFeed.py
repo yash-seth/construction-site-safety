ab530a84c48ef2c2f027d42e6c1577896e32ba2f
Yash Seth <yashseth2002@gmail.com>
2024-02-13 17:36:10 +0530

    feat: coordinates of worker without helmets returned with alert

--- FeedChecker/checkFeed.py
+++ FeedChecker/checkFeed.py
@@ -15,8 +15,13 @@ def getOverlap(x1, x2, y1, y2, a1, a2, b1, b2):
 
     top_coordinate = [max(a1, x1), max(b1, y1)]
     bottom_coordinate = [min(a2, x2), min(b2, y2)]
+
     AreaOfOverlap = (bottom_coordinate[0] - top_coordinate[0]) * (bottom_coordinate[1] - top_coordinate[1])
+
+    # total area of helmet
     AreaOfHelmet = (b2 - b1) * (a2 - a1)
+
+    # area of overlap over total area of helmet gives % of helmet that lies in the bounding box of the person
     PercentageOverlap = (AreaOfOverlap / AreaOfHelmet) * 100
     return PercentageOverlap
 
@@ -25,10 +30,12 @@ property_id = int(cv2.CAP_PROP_FRAME_COUNT)
 length = int(cv2.VideoCapture.get(vid, property_id)) 
 print("Number of frames in the video: ", length) 
 
+frameCounter = 0
 while True:
+    frameCounter += 1
     successful, frame = vid.read()
     if successful:
-        print("\nNew Frame")
+        print("\nFrame ", frameCounter)
         # identifying the helmets in the current frame
         Helmet_Model = YOLO('../backend/yolov8_helmet_model.pt')
         Helmet_Results = Helmet_Model(frame)
@@ -64,6 +71,7 @@ while True:
                 print("Overlap percentage: ", PercentageOverlap)
                 if PercentageOverlap < 80:
                     print("Worker without helmet spotted!")
+                    print(f"Bounding box of worker without helmet: (x1, y1): ({x1}, {y1}), (x2, y2): ({x2}, {y2})")
     else:
         break
         
