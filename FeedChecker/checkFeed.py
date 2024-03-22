295524a1689fe60dba813e35eabfc7df3e5534d0
Yash Seth <yashseth2002@gmail.com>
2024-03-22 23:08:58 +0530

    feat: add optimization for checking every alternate frame

--- FeedChecker/checkFeed.py
+++ FeedChecker/checkFeed.py
@@ -5,7 +5,7 @@ from ultralytics.utils.plotting import Annotator
 import numpy as np
 
 # Define path to video file
-source = './combined_short.mp4'
+source = './worker_with_helmet_multiple.mp4'
 
 # return percentage of the helmet [a1, a2, b1, b2] which lies within the bounding box of the identified person [x1, x2, y1, y2]
 def getOverlap(x1, x2, y1, y2, a1, a2, b1, b2):
@@ -43,43 +43,50 @@ while True:
     frameCounter += 1
     successful, frame = vid.read()
     if successful:
-        print("\nFrame ", frameCounter)
-        # identifying the helmets in the current frame
-        Helmet_Model = YOLO('../backend/yolov8_helmet_model.pt')
-        Helmet_Results = Helmet_Model(frame)
-        helmet_boxes = Helmet_Results[0].boxes.xyxy.tolist()
-        helmet_classes = Helmet_Results[0].boxes.cls.tolist()
-        helmet_names = Helmet_Results[0].names
-        helmet_confidences = Helmet_Results[0].boxes.conf.tolist()
+        if frameCounter % 2 == 0:
+            print("\nFrame ", frameCounter)
+            # identifying the helmets in the current frame
+            Helmet_Model = YOLO('../backend/yolov8_helmet_model.pt')
+            Helmet_Results = Helmet_Model(frame)
+            helmet_boxes = Helmet_Results[0].boxes.xyxy.tolist()
+            helmet_classes = Helmet_Results[0].boxes.cls.tolist()
+            helmet_names = Helmet_Results[0].names
+            helmet_confidences = Helmet_Results[0].boxes.conf.tolist()
 
-        # identifying the people, PPE kits and face masks in the current frame
-        PPE_FaceMask_Model = YOLO('../backend/multiclass-yolov8.pt')
-        PPE_FaceMask_Results = PPE_FaceMask_Model(frame)
-        PPE_FaceMask_boxes = PPE_FaceMask_Results[0].boxes.xyxy.tolist()
-        PPE_FaceMask_classes = PPE_FaceMask_Results[0].boxes.cls.tolist()
-        PPE_FaceMask_names = PPE_FaceMask_Results[0].names
-        PPE_FaceMask_confidences = PPE_FaceMask_Results[0].boxes.conf.tolist()
-        
-        print()
+            # identifying the people, PPE kits and face masks in the current frame
+            PPE_FaceMask_Model = YOLO('../backend/multiclass-yolov8.pt')
+            PPE_FaceMask_Results = PPE_FaceMask_Model(frame)
+            PPE_FaceMask_boxes = PPE_FaceMask_Results[0].boxes.xyxy.tolist()
+            PPE_FaceMask_classes = PPE_FaceMask_Results[0].boxes.cls.tolist()
+            PPE_FaceMask_names = PPE_FaceMask_Results[0].names
+            PPE_FaceMask_confidences = PPE_FaceMask_Results[0].boxes.conf.tolist()
+            
+            print()
 
-        for box, cls, conf in zip(PPE_FaceMask_boxes, PPE_FaceMask_classes, PPE_FaceMask_confidences):
-            x1, y1, x2, y2 = box
-            confidence = conf
-            detected_class = cls
-            name = PPE_FaceMask_names[int(cls)]
+            for box, cls, conf in zip(PPE_FaceMask_boxes, PPE_FaceMask_classes, PPE_FaceMask_confidences):
+                x1, y1, x2, y2 = box
+                confidence = conf
+                detected_class = cls
+                name = PPE_FaceMask_names[int(cls)]
 
-            # person is class 5.0
-            if detected_class == 5.0:
-                PercentageOverlap = 0
-                for box1, cls1, conf1 in zip(helmet_boxes, helmet_classes, helmet_confidences):
-                    # helmet is class 0.0
-                    if cls1 != 0.0: continue
-                    a1, b1, a2, b2 = box1
-                    PercentageOverlap = max(PercentageOverlap, getOverlap(x1, x2, y1, y2, a1, a2, b1, b2))
-                print("Overlap percentage: ", PercentageOverlap)
-                if PercentageOverlap < 80:
-                    print("Worker without helmet spotted!")
-                    print(f"Bounding box of worker without helmet: (x1, y1): ({x1}, {y1}), (x2, y2): ({x2}, {y2})\n")
+                # person is class 5.0
+                if detected_class == 5.0:
+                    # cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 2)
+                    PercentageOverlap = 0
+                    for box1, cls1, conf1 in zip(helmet_boxes, helmet_classes, helmet_confidences):
+                        # helmet is class 0.0
+                        if cls1 != 0.0: continue
+                        a1, b1, a2, b2 = box1
+                        # cv2.rectangle(frame, (int(a1), int(b1)), (int(a2), int(b2)), (0, 255, 0), 2)
+                        PercentageOverlap = max(PercentageOverlap, getOverlap(x1, x2, y1, y2, a1, a2, b1, b2))
+                    print("Overlap percentage: ", PercentageOverlap)
+                    if PercentageOverlap < 80:
+                        print("Worker without helmet spotted!")
+                        print(f"Bounding box of worker without helmet: (x1, y1): ({x1}, {y1}), (x2, y2): ({x2}, {y2})\n")
+            # cv2.imshow("Frame", frame)
+            # cv2.waitKey(0)
+        else:
+            continue
     else:
         break
         
